e2f3f3ae1cba20b9fe282d9ba1c227493ec84b3f
Isma Navarro <ismanapa@gmail.com>
2023-02-07 13:05:39 +0100

    feat(03-2): promises finally and catch

---
+++ src/03-promises-in-deep/2-promise-chain/promise-catch-chain.js
@@ -0,0 +1,24 @@
+import { appendFile, copyFile, unlink, writeFile } from "node:fs/promises";
+import * as path from "path";
+
+import { getUsersFromApi } from "./getUsersFromApi.js";
+
+const file = path.resolve("./users.json");
+const dest = process.argv[2] || "../dest.json";
+
+writeFile(file, "")
+  .then(getUsersFromApi)
+  .then((users) => {
+    return appendFile(file, JSON.stringify(users));
+  })
+  .then(() => {
+    return copyFile(file, dest);
+  })
+  .then(() => console.log("Process finished successfully! 🎉"))
+  .finally(() => unlink(file))
+  .catch(() => {
+    console.error("Error in file processing 😢");
+  })
+  .then(() => {
+    console.log("Continue promise chain ⛓️");
+  });
---
+++ src/03-promises-in-deep/2-promise-chain/promise-chain.js
@@ -0,0 +1,23 @@
+import { appendFile, copyFile, unlink, writeFile } from "node:fs/promises";
+import * as path from "path";
+
+import { getUsersFromApi } from "./getUsersFromApi.js";
+
+const file = path.resolve("./users.json");
+const dest = process.argv[2] || "../dest.json";
+
+writeFile(file, "")
+  .then(getUsersFromApi)
+  .then((users) => {
+    return appendFile(file, JSON.stringify(users));
+  })
+  .then(() => {
+    return copyFile(file, dest);
+  })
+  .then(() => console.log("Process finished successfully! 🎉"))
+  // Finally will be executed always
+  .finally(() => unlink(file))
+  // Catch only executes when there is an error
+  .catch(() => {
+    console.error("Error in file processing 😢");
+  });
