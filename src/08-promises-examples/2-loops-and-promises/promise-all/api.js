9effdab6503aac4695db691618fd2b167aef24c9
Isma Navarro <ismanapa@gmail.com>
2023-02-21 11:22:30 +0100

    feat(08-2): promise array with promise all

---
+++ src/08-promises-examples/2-loops-and-promises/promise-all/api.js
@@ -0,0 +1,23 @@
+export function searchTotalCustomers() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(500);
+    }, 1000);
+  });
+}
+
+export function searchMonthlyRevenue() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("10.000€");
+    }, 500);
+  });
+}
+
+export function searchOpenIncidents() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(10);
+    }, 2000);
+  });
+}
---
+++ src/08-promises-examples/2-loops-and-promises/promise-all/promiseAll.js
@@ -0,0 +1,22 @@
+import {
+  searchTotalCustomers,
+  searchMonthlyRevenue,
+  searchOpenIncidents,
+} from "./api.js";
+
+const printCustomerMetric = (wrapperId, query) => async () => {
+  const wrapper = document.getElementById(wrapperId);
+  const result = await query();
+
+  wrapper.querySelector(".loader").remove();
+  wrapper.querySelector(".metric").innerHTML = result;
+};
+
+/*
+ With a for loop the promises are resolved sequentially
+*/
+await Promise.all([
+  printCustomerMetric("customers", searchTotalCustomers)(),
+  printCustomerMetric("revenue", searchMonthlyRevenue)(),
+  printCustomerMetric("incidents", searchOpenIncidents)(),
+]);
