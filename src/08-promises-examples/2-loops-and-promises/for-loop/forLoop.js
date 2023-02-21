4154d273c74b452a500ec1355e5e38e9741dd637
Isma Navarro <ismanapa@gmail.com>
2023-02-21 11:16:28 +0100

    feat(08-2): promise array with for loop

---
+++ src/08-promises-examples/2-loops-and-promises/for-loop/forLoop.js
@@ -0,0 +1,27 @@
+import {
+  searchTotalCustomers,
+  searchMonthlyRevenue,
+  searchOpenIncidents,
+} from "./api.js";
+
+const printCustomerMetric = (wrapperId, query) => async () => {
+  const wrapper = document.getElementById(wrapperId);
+  const result = await query();
+
+  wrapper.querySelector(".loader").remove();
+  wrapper.querySelector(".metric").innerHTML = result;
+};
+
+const metricQueries = [
+  printCustomerMetric("customers", searchTotalCustomers),
+  printCustomerMetric("revenue", searchMonthlyRevenue),
+  printCustomerMetric("incidents", searchOpenIncidents),
+];
+
+/*
+ With a for loop the promises are resolved sequentially
+*/
+for (let index = 0; index < metricQueries.length; index++) {
+  const metricQuery = metricQueries[index];
+  await metricQuery();
+}
