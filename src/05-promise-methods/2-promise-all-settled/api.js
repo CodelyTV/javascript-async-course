9065bc112d91522d8e41f53d010cededa19d4ab6
Isma Navarro <ismanapa@gmail.com>
2023-02-21 12:15:29 +0100

    feat(05-2): promise all settled example

---
+++ src/05-promise-methods/2-promise-all-settled/api.js
@@ -0,0 +1,18 @@
+export function getUserInfo() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: "Núria",
+        email: "nuriamail@mail.com",
+      });
+    }, 2000);
+  });
+}
+
+export function getUserEnrolledCourses() {
+  return new Promise((_resolve, reject) => {
+    setTimeout(() => {
+      reject("Error fetching enrolled courses");
+    }, 2000);
+  });
+}
---
+++ src/05-promise-methods/2-promise-all-settled/promiseAllSettled.js
@@ -0,0 +1,34 @@
+import { getUserEnrolledCourses, getUserInfo } from "./api.js";
+
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
+const courses = document.getElementById("courses");
+const errors = document.getElementById("errors");
+
+Promise.allSettled([getUserInfo(), getUserEnrolledCourses()]).then(
+  (promisesResults) => {
+    const [userInfoPromiseResult, enrolledCoursesPromiseResult] =
+      promisesResults;
+
+    if (userInfoPromiseResult.status === "fulfilled") {
+      userName.innerHTML = userInfoPromiseResult.value.name;
+      userEmail.innerHTML = userInfoPromiseResult.value.email;
+    }
+
+    if (enrolledCoursesPromiseResult.status === "fulfilled") {
+      enrolledCoursesPromiseResult.value.forEach((course) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = course.title;
+        courses.appendChild(listItem);
+      });
+    }
+
+    promisesResults
+      .filter((r) => r.status === "rejected")
+      .forEach((r) => {
+        const error = document.createElement("li");
+        error.innerHTML = r.reason;
+        errors.appendChild(error);
+      });
+  }
+);
