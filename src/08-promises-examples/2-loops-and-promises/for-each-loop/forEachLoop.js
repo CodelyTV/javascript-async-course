7f4be56d804d386d2e821e526acd80031c54859f
Isma Navarro <ismanapa@gmail.com>
2023-02-21 11:16:47 +0100

    feat(08-2): promise array with forEach loop

---
+++ src/08-promises-examples/2-loops-and-promises/for-each-loop/forEachLoop.js
@@ -0,0 +1,26 @@
+import {
+  searchTotalCustomers,
+  searchMonthlyRevenue,
+  searchOpenIncidents,
+} from "./api.js";
+
+const printCustomerMetric = (wrapperId, query) => async () => {
+  const wrapper = document.getElementById(wrapperId);
+  const result = await query();
+
+  wrapper.querySelector(".loader").remove();
+  wrapper.querySelector(".metric").innerHTML = result;
+};
+
+const metricQueries = [
+  printCustomerMetric("customers", searchTotalCustomers),
+  printCustomerMetric("revenue", searchMonthlyRevenue),
+  printCustomerMetric("incidents", searchOpenIncidents),
+];
+
+/*
+ With a forEach loop the promises are resolved in parallel
+*/
+metricQueries.forEach((metricQuery) => {
+  metricQuery();
+});
