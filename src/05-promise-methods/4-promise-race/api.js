b48dc37f0b2b8230bd4e5d1902902d41b7013bde
Isma Navarro <ismanapa@gmail.com>
2023-03-07 12:04:02 +0100

    feat(05-3): promise.race

---
+++ src/05-promise-methods/4-promise-race/api.js
@@ -0,0 +1,11 @@
+export function getUserList() {
+  return fetch("https://hub.dummyapis.com/delay?seconds=5");
+}
+
+export function timeoutCheck(timeout) {
+  return new Promise((_resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error("La solicitud ha tardado demasiado en completarse."));
+    }, timeout);
+  });
+}
---
+++ src/05-promise-methods/4-promise-race/promiseRace.js
@@ -0,0 +1,11 @@
+import { getUserList, timeoutCheck } from "./api.js";
+
+const timeout = process.argv[2] || 5000;
+
+Promise.race([getUserList(), timeoutCheck(timeout)])
+  .then(() => {
+    console.log("🚀 Users found");
+  })
+  .catch(() => {
+    console.log("⌛ Timeout error in getUserList");
+  });
